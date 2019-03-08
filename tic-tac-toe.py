7d8e8d04b1b1d3d0f5eed29e841ecab5bd68b3b5
Rohit Pandey <rohitpandey357@gmail.com>
2019-03-08 11:48:32 +0530

    final program

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -11,66 +11,76 @@ def printBoard(board):
     print(board['7'] + '  | ' + board['8'] + ' | ' + board['9'])
 
 turn = 'X'
-for i in range(9):
+while theBoard['1']=='' or  theBoard['2']=='' or  theBoard['3']=='' or  theBoard['4']=='' or  theBoard['5']=='' or  theBoard['6']=='' or  theBoard['7']=='' or  theBoard['8']=='' or  theBoard['9']=='' :
     printBoard(theBoard)
     print('\n\nTurn for ' + turn + '. Move on which space? (1-9) \n')
     move = input()
-    if (theBoard[move]==''):
-        theBoard[move] = turn
-    else:
-         print('Enter a valid move.')
-    
     if move not in theBoard:
-        print('Enter a valid move.')
+        print('\nEnter a valid move.')
+
+    elif (theBoard[move]==''):
+        theBoard[move] = turn
+        
     else:
-        if turn == 'X':
-            turn = 'O'
-        else:
-            turn = 'X'  
+         print('\nEnter a valid move.')
+    
+    
+    
     if theBoard['1']!='' and theBoard['2']==theBoard['1'] and theBoard['3']==theBoard['2']:
-                print(turn+ ' Won!')
                 printBoard(theBoard)
+                print(turn+ ' Won!')
                 time.sleep(10)
                 exit()
     
     if theBoard['4']!='' and theBoard['5']==theBoard['4'] and theBoard['6']==theBoard['5']:
-                print(turn+ ' Won!')
                 printBoard(theBoard)
+                print(turn+ ' Won!')
                 time.sleep(10)
                 exit()
     
     if theBoard['7']!='' and theBoard['8']==theBoard['7'] and theBoard['9']==theBoard['8']:
-                print(turn+ ' Won!')
                 printBoard(theBoard)
+                print(turn+ ' Won!')
                 time.sleep(10)
                 exit()
 
     if theBoard['1']!='' and theBoard['4']==theBoard['1'] and theBoard['7']==theBoard['4']:
-                print(turn+ ' Won!')
                 printBoard(theBoard)
+                print(turn+ ' Won!')
                 time.sleep(10)
                 exit()   
 
     if theBoard['2']!='' and theBoard['5']==theBoard['2'] and theBoard['8']==theBoard['5']:
-                print(turn+ ' Won!')
                 printBoard(theBoard)
+                print(turn+ ' Won!')
                 time.sleep(10)
                 exit() 
 
     if theBoard['3']!='' and theBoard['6']==theBoard['3'] and theBoard['9']==theBoard['6']:
-                print(turn+ ' Won!')
                 printBoard(theBoard)
+                print(turn+ ' Won!')
                 time.sleep(10)
                 exit()            
 
     if theBoard['1']!='' and theBoard['5']==theBoard['1'] and theBoard['9']==theBoard['5']:
-                    print(turn+ ' Won!')
                     printBoard(theBoard)
+                    print(turn+ ' Won!')
                     time.sleep(10)
                     exit()
     if theBoard['3']!='' and theBoard['5']==theBoard['3'] and theBoard['7']==theBoard['5']:
+                    printBoard(theBoard)
                     print(turn+ ' Won!')
-                    printBoard(theBoard)                     
                     time.sleep(10)
                     exit()
 
+    if theBoard['1']!='' and  theBoard['2']!='' and  theBoard['3']!='' and  theBoard['4']!='' and  theBoard['5']!='' and  theBoard['6']!='' and  theBoard['7']!='' and  theBoard['8']!='' and  theBoard['9']!='' :
+        printBoard(theBoard)
+        print('\nMatch Draw!')
+        time.sleep(10)
+        exit()
+
+    if theBoard[move]==turn:
+        if turn == 'X':
+                turn = 'O'
+        else:
+                turn = 'X'     
